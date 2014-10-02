646b44685b0ee99a13f4c2e3a8021b329a8b121b
Andrew Martin <andrew.martin@ncl.ac.uk>
2014-10-02 11:56:54 +0100

    * Android successfully writes to file and then reads out what was just written

--- www/index.js
+++ www/index.js
@@ -1,18 +1,29 @@
 document.addEventListener('deviceready',function(){
-    
+    /* USEFUL STUFF:
+     * https://developer.mozilla.org/en-US/docs/Web/API/FileError#Error_codes
+     */
     var fail = function(e){console.log("oh dear, something went wrong :(");};
     var filePersist = new FilePersist();
-    filePersist.getDir(cordova.file.dataDirectory,function(dataDir){
-        /* USEFUL STUFF:
-         * https://developer.mozilla.org/en-US/docs/Web/API/FileError#Error_codes
-         */
-
-        filePersist.getDir(dataDir,function(myExampleDir){
-            filePersist.getFileContents(myExampleDir,"exampleFile",function(obj){
-                console.log("What is in "+myExampleDir.nativeURL+"exampleFile?");
-                console.log(obj);
-            },fail,filePersist.options.createIfDoesNotExist);
-        },fail,"exampleDir",filePersist.options.createIfDoesNotExist);
-    },fail);
+    var myExampleDirName = "exampleDir";
+    var myExampleFileName = "exampleFile";
     
+    filePersist.getDir(cordova.file.dataDirectory,function(dataDir){
+        filePersist.getDir(dataDir,function(myExampleDir){
+            var objToSave = {
+                "string": "hello",
+                "boolean": true,
+                "int": 1,
+                "array": ["test","test2"]
+            };
+            var success = function(){
+                console.log("Object successfully saved to fileSystem");
+                filePersist.getFileContents(myExampleDir,myExampleFileName,function(obj){
+                    console.log("What is in "+myExampleDir.nativeURL+"exampleFile?");
+                    console.log(obj);
+                },fail,filePersist.options.createIfDoesNotExist);
+            };
+            filePersist.setFileContents(myExampleDir,myExampleFileName,objToSave,fail,success,filePersist.options.createIfDoesNotExist);
+        },fail,myExampleDirName,filePersist.options.createIfDoesNotExist);
+    },fail);
+
 },false);
