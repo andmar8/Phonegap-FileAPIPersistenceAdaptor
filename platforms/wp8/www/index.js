5e3e3b72847eea19611c16664184f06e0383902e
Andrew Martin <andrew.martin@ncl.ac.uk>
2014-10-02 12:30:14 +0100

    * windows phone 8 being awkward about something?

--- platforms/wp8/www/index.js
+++ platforms/wp8/www/index.js
@@ -1,5 +1,34 @@
 document.addEventListener('deviceready',function(){
-    console.log("Hello!");
+    /* USEFUL STUFF:
+     * https://developer.mozilla.org/en-US/docs/Web/API/FileError#Error_codes
+     */
+    var fail = function(e){
+        navigator.notification.alert("oh dear, something went wrong :(",function(){},"File Persistance","OK");
+        console.log("oh dear, something went wrong :(");
+    };
+    var filePersist = new FilePersist();
+    var myExampleDirName = "exampleDir";
+    var myExampleFileName = "exampleFile";
     
-    
+    filePersist.getDir(cordova.file.dataDirectory,function(dataDir){
+        filePersist.getDir(dataDir,function(myExampleDir){
+            var objToSave = {
+                "string": "hello",
+                "boolean": true,
+                "int": 1,
+                "array": ["test","test2"]
+            };
+            var success = function(){
+                navigator.notification.alert("Object successfully saved to fileSystem",function(){},"File Persistance","OK");
+                console.log("Object successfully saved to fileSystem");
+                filePersist.getFileContents(myExampleDir,myExampleFileName,function(obj){
+                    navigator.notification.alert("The console log is now displaying what we just saved...",function(){},"File Persistance","OK");
+                    console.log("What is in "+myExampleDir.nativeURL+myExampleFileName);
+                    console.log(obj);
+                },fail,filePersist.options.createIfDoesNotExist);
+            };
+            filePersist.setFileContents(myExampleDir,myExampleFileName,objToSave,fail,success,filePersist.options.createIfDoesNotExist);
+        },fail,myExampleDirName,filePersist.options.createIfDoesNotExist);
+    },fail);
+
 },false);
