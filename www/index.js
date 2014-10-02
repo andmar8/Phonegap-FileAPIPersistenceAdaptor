4e6c880e5cc4e9f8745773a07702016b0af39c63
Andrew Martin <andrew.martin@ncl.ac.uk>
2014-10-02 11:38:05 +0100

    * reading out file in android

--- www/index.js
+++ www/index.js
@@ -1,5 +1,18 @@
 document.addEventListener('deviceready',function(){
-    console.log("Hello!");
     
+    var fail = function(e){console.log("oh dear, something went wrong :(");};
+    var filePersist = new FilePersist();
+    filePersist.getDir(cordova.file.dataDirectory,function(dataDir){
+        /* USEFUL STUFF:
+         * https://developer.mozilla.org/en-US/docs/Web/API/FileError#Error_codes
+         */
+
+        filePersist.getDir(dataDir,function(myExampleDir){
+            filePersist.getFileContents(myExampleDir,"exampleFile",function(obj){
+                console.log("What is in "+myExampleDir.nativeURL+"exampleFile?");
+                console.log(obj);
+            },fail,filePersist.options.createIfDoesNotExist);
+        },fail,"exampleDir",filePersist.options.createIfDoesNotExist);
+    },fail);
     
 },false);
